c6b90ede0a0554e980f782ba84a90bd3244d7a2c
vlas237 <83783001+vlas237@users.noreply.github.com>
2023-05-18 21:49:37 -0300

    json changes

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <div>
         <Navbar />
         <Routes>
-        <Route path="/" element={<Home />} />
+        <Route exact path="/" component={Home} />
           <Route path="/about" element={<About />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
