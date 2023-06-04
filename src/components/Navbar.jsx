08a40f7c607ca1b0e1611031ebfb3f2d868d706a
vlas237 <83783001+vlas237@users.noreply.github.com>
2023-06-04 17:08:58 -0300

    color change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -68,7 +68,7 @@ const Navbar = () => {
             >
               <span
                 className={`transition-colors duration-200 ${
-                  activeSection === "home" ? "text-[#fd004f]" : ""
+                  activeSection === "home" ? "text-blue-500" : ""
                 }`}
               >
                 <FaHome className="inline-block pb-1 mr-2" size={20} />
@@ -84,7 +84,7 @@ const Navbar = () => {
             >
               <span
                 className={`transition-colors duration-200 ${
-                  activeSection === "about" ? "text-[#fd004f]" : ""
+                  activeSection === "about" ? "text-blue-500" : ""
                 }`}
               >
                 <FaUser className="inline-block pb-1 mr-2" size={20} />
@@ -100,7 +100,7 @@ const Navbar = () => {
             >
               <span
                 className={`transition-colors duration-200 ${
-                  activeSection === "projects" ? "text-[#fd004f]" : ""
+                  activeSection === "projects" ? "text-blue-500" : ""
                 }`}
               >
                 <FaBriefcase className="inline-block pb-1 mr-2" size={20} />
@@ -116,7 +116,7 @@ const Navbar = () => {
             >
               <span
                 className={`transition-colors duration-200 ${
-                  activeSection === "contact" ? "text-[#fd004f]" : ""
+                  activeSection === "contact" ? "text-blue-500" : ""
                 }`}
               >
                 <FaPhone className="inline-block pb-1 mr-2" size={20} />
@@ -131,11 +131,11 @@ const Navbar = () => {
         >
           {darkMode ? (
             <div className="flex items-center justify-center">
-              <IoIosSunny className="md:h-6 md:w-6 h-8 w-8 text-gray-300 hover:text-[#fd004f]  transition-colors duration-200" />
+              <IoIosSunny className="md:h-6 md:w-6 h-8 w-8 text-gray-300 hover:text-blue-500  transition-colors duration-200" />
             </div>
           ) : (
             <div className="flex items-center justify-center">
-              <IoIosMoon className="md:h-6 md:w-6 h-8 w-8 text-gray-500  hover:text-[#fd004f] transition-colors duration-200" />
+              <IoIosMoon className="md:h-6 md:w-6 h-8 w-8 text-gray-500  hover:text-blue-500 transition-colors duration-200" />
             </div>
           )}
         </button>
@@ -172,7 +172,7 @@ const Navbar = () => {
           >
             <span
               className={`transition-colors duration-200 ${
-                activeSection === "home" ? "text-[#fd004f]" : ""
+                activeSection === "home" ? "text-blue-500" : ""
               }`}
             >
               <FaHome className="inline-block mr-2 pb-1" size={30} />
@@ -189,7 +189,7 @@ const Navbar = () => {
           >
             <span
               className={`transition-colors duration-200 ${
-                activeSection === "about" ? "text-[#fd004f]" : ""
+                activeSection === "about" ? "text-blue-500" : ""
               }`}
             >
               <FaUser className="inline-block mr-2 pb-1" size={30} />
@@ -206,7 +206,7 @@ const Navbar = () => {
           >
             <span
               className={`transition-colors duration-200 ${
-                activeSection === "projects" ? "text-[#fd004f]" : ""
+                activeSection === "projects" ? "text-blue-500" : ""
               }`}
             >
               <FaBriefcase className="inline-block mr-2 pb-1" size={30} />
@@ -223,7 +223,7 @@ const Navbar = () => {
           >
             <span
               className={`transition-colors duration-200 ${
-                activeSection === "contact" ? "text-[#fd004f]" : ""
+                activeSection === "contact" ? "text-blue-500" : ""
               }`}
             >
               <FaPhone className="inline-block mr-2 pb-1" size={30} />
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -46,7 +46,7 @@ const Projects = () => {
                 href="https://vlas-dev.github.io/calculator-app"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="text-center px-4 py-2 m-2  text-white border-2 border-[#fd004f] hover:dark:border-white bg-[#fd004f] hover:bg-white hover:text-gray-600 font-bold text-lg transition-colors duration-200"
+                className="text-center px-4 py-2 m-2  text-white border-2 border-blue-500 hover:dark:border-white bg-blue-500 hover:bg-white hover:text-gray-600 font-bold text-lg transition-colors duration-200"
               >
                 Live
               </a>
@@ -54,7 +54,7 @@ const Projects = () => {
                 href="https://github.com/vlas-dev/calculator-app"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="text-center px-4 py-2 m-2  text-white border-2 border-[#fd004f] hover:dark:border-white bg-[#fd004f] hover:bg-white hover:text-gray-600 font-bold text-lg transition-colors duration-200"
+                className="text-center px-4 py-2 m-2  text-white border-2 border-blue-500 hover:dark:border-white bg-blue-500 hover:bg-white hover:text-gray-600 font-bold text-lg transition-colors duration-200"
               >
                 Code
               </a>
@@ -77,7 +77,7 @@ const Projects = () => {
                 href="https://google.com"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="text-center px-4 py-2 m-2  text-white border-2 border-[#fd004f] hover:dark:border-white bg-[#fd004f] hover:bg-white hover:text-gray-600 font-bold text-lg transition-colors duration-200"
+                className="text-center px-4 py-2 m-2  text-white border-2 border-blue-500 hover:dark:border-white bg-blue-500 hover:bg-white hover:text-gray-600 font-bold text-lg transition-colors duration-200"
               >
                 Live
               </a>
@@ -85,7 +85,7 @@ const Projects = () => {
                 href="https://google.com"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="text-center px-4 py-2 m-2  text-white border-2 border-[#fd004f] hover:dark:border-white bg-[#fd004f] hover:bg-white hover:text-gray-600 font-bold text-lg transition-colors duration-200"
+                className="text-center px-4 py-2 m-2  text-white border-2 border-blue-500 hover:dark:border-white bg-blue-500 hover:bg-white hover:text-gray-600 font-bold text-lg transition-colors duration-200"
               >
                 Code
               </a>
@@ -108,7 +108,7 @@ const Projects = () => {
                 href="https://google.com"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="text-center px-4 py-2 m-2  text-white border-2 border-[#fd004f] hover:dark:border-white bg-[#fd004f] hover:bg-white hover:text-gray-600 font-bold text-lg transition-colors duration-200"
+                className="text-center px-4 py-2 m-2  text-white border-2 border-blue-500 hover:dark:border-white bg-blue-500 hover:bg-white hover:text-gray-600 font-bold text-lg transition-colors duration-200"
               >
                 Live
               </a>
@@ -116,7 +116,7 @@ const Projects = () => {
                 href="https://google.com"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="text-center px-4 py-2 m-2  text-white border-2 border-[#fd004f] hover:dark:border-white bg-[#fd004f] hover:bg-white hover:text-gray-600 font-bold text-lg transition-colors duration-200"
+                className="text-center px-4 py-2 m-2  text-white border-2 border-blue-500 hover:dark:border-white bg-blue-500 hover:bg-white hover:text-gray-600 font-bold text-lg transition-colors duration-200"
               >
                 Code
               </a>
